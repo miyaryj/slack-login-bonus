613bcacbe49dd2c0d3323efc82a755a071d086e2
miyaryj <miya.ryj@gmail.com>
2023-02-14 14:26:14 +0900

    List huddle users

--- index.js
+++ index.js
@@ -8,33 +8,39 @@ const app = new App({
 });
 
 exports.handler = async (event) => {
+  console.log(event);
+  const { dryRun } = event;
   const today = moment().startOf("day");
 
   try {
-    const chatUsers = await getChatUsers(today);
-    const huddleUsers = []; // TODO
+    const { chatUsers, huddleUsers } = await getUsers(today);
 
     const messages = [
       "Today's login users",
       "on chat: " + chatUsers.map((u) => `<@${u}>`).join(" "),
       "on huddle: " + huddleUsers.map((u) => `<@${u}>`).join(" "),
     ];
-    const result = await app.client.chat.postMessage({
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: process.env.TARGET_CHANNEL,
-      text: messages.join("\n"),
-    });
-    console.log(result);
+    console.log(messages);
+
+    if (!dryRun) {
+      const result = await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: process.env.TARGET_CHANNEL,
+        text: messages.join("\n"),
+      });
+      console.log(result);
+    }
   } catch (error) {
     console.error(error);
   }
 };
 
-const getChatUsers = async (today) => {
+const getUsers = async (today) => {
   const result = await app.client.conversations.history({
     token: process.env.SLACK_BOT_TOKEN,
     channel: process.env.TARGET_CHANNEL,
     oldest: today.unix(),
+    include_all_metadata: true,
   });
   console.log(result);
 
@@ -43,12 +49,22 @@ const getChatUsers = async (today) => {
     .map((m) => {
       return m.user;
     })
-    .filter((u) => u)
+    .filter((u) => u && u != "USLACKBOT")
     .reduce((uniq, a) => {
       if (uniq.indexOf(a) < 0) uniq.push(a);
       return uniq;
     }, []);
   console.log(chatUsers);
 
-  return chatUsers;
-}
+  const huddleUsers = result.messages
+    .filter((m) => m.user == "USLACKBOT" && m.room)
+    .map((m) => m.room.participant_history)
+    .flat()
+    .reduce((uniq, a) => {
+      if (uniq.indexOf(a) < 0) uniq.push(a);
+      return uniq;
+    }, []);
+  console.log(huddleUsers);
+
+  return { chatUsers, huddleUsers };
+};
