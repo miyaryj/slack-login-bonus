d6bb97435ae5971939cfde38f465b4495f152850
miyaryj <miya.ryj@gmail.com>
2023-03-07 11:50:58 +0900

    Get user history

--- cli.js
+++ cli.js
@@ -1,10 +1,14 @@
 const { handler } = require("./index");
 const commandLineArgs = require("command-line-args");
 
-const optionDefinitions = [{ name: "dry-run", alias: "d", type: Boolean }];
+const optionDefinitions = [
+  { name: "history", alias: "h", type: Boolean },
+  { name: "dry-run", alias: "d", type: Boolean },
+];
 
 const options = commandLineArgs(optionDefinitions);
 
 handler({
+  history: options["history"],
   dryRun: options["dry-run"],
 });
--- index.js
+++ index.js
@@ -7,14 +7,51 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+const isUserMessage = (message) => {
+  return (
+    !message.bot_id &&
+    !message.app_id &&
+    message.user &&
+    message.user != "USLACKBOT"
+  );
+};
+
+const isHuddleMessage = (message) => {
+  return message.user == "USLACKBOT" && message.room;
+};
+
+const groupby = (array, by) => {
+  return Array.from(
+    array.reduce((map, cur, idx, src) => {
+      const key = cur[by];
+      const list = map.get(key);
+      if (list) {
+        list.push(cur);
+      } else {
+        map.set(key, [cur]);
+      }
+      return map;
+    }, new Map())
+  );
+};
+
 exports.handler = async (event) => {
   console.log(event);
-  const { dryRun } = event;
+  const { history, dryRun } = event;
+
+  if (history) {
+    await reportHistory(dryRun);
+  } else {
+    await reportToday(dryRun);
+  }
+};
+
+const reportToday = async (dryRun) => {
   const today = DateTime.now().setZone('Asia/Tokyo').startOf("day");
   console.log("today: ", today.toISO());
 
   try {
-    const { chatUsers, huddleUsers } = await getUsers(today);
+    const { chatUsers, huddleUsers } = await getLoginUsers(today);
 
     const chatUserInfos = await getUserInfos(chatUsers);
     const chatUserNames = chatUserInfos.map(
@@ -56,7 +93,33 @@ exports.handler = async (event) => {
   }
 };
 
-const getUsers = async (today) => {
+const reportHistory = async (dryRun) => {
+  const sinceStr = process.env.HISTORY_SINCE;
+  if (!sinceStr) {
+    console.error("No HISTORY_SINCE");
+    return;
+  }
+  const since = DateTime.fromISO(sinceStr).setZone('Asia/Tokyo').startOf("day");
+  console.log("since: ", since.toISO());
+
+  try {
+    const histories = await getHistories(since);
+    const userHistories = groupby(histories, "user");
+    userHistories.forEach(([user, histories]) => {
+      const loginDates = histories
+        .map((h) => h.date)
+        .reduce((uniq, a) => {
+          if (uniq.indexOf(a) < 0) uniq.push(a);
+          return uniq;
+        }, []);
+      console.log(`${user}: ${loginDates}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getLoginUsers = async (today) => {
   const result = await app.client.conversations.history({
     token: process.env.SLACK_BOT_TOKEN,
     channel: process.env.TARGET_CHANNEL,
@@ -67,11 +130,10 @@ const getUsers = async (today) => {
 
   const chatUsers = result.messages
     .reverse()
-    .filter((m) => !m.bot_id && !m.app_id)
+    .filter((m) => isUserMessage(m))
     .map((m) => {
       return m.user;
     })
-    .filter((u) => u && u != "USLACKBOT")
     .reduce((uniq, a) => {
       if (uniq.indexOf(a) < 0) uniq.push(a);
       return uniq;
@@ -79,7 +141,7 @@ const getUsers = async (today) => {
   console.log(chatUsers);
 
   const huddleUsers = result.messages
-    .filter((m) => m.user == "USLACKBOT" && m.room)
+    .filter((m) => isHuddleMessage(m))
     .map((m) => m.room.participant_history)
     .flat()
     .reduce((uniq, a) => {
@@ -89,6 +151,55 @@ const getUsers = async (today) => {
   console.log(huddleUsers);
 
   return { chatUsers, huddleUsers };
+};
+
+const getHistories = async (since) => {
+  const result = await app.client.conversations.history({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: process.env.TARGET_CHANNEL,
+    oldest: since.toUnixInteger(),
+    include_all_metadata: true,
+  });
+  const messages = result.messages;
+  let cursor = result.response_metadata.next_cursor;
+  while (cursor) {
+    const nextResult = await app.client.conversations.history({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: process.env.TARGET_CHANNEL,
+      oldest: since.toUnixInteger(),
+      cursor: cursor,
+      include_all_metadata: true,
+    });
+    messages.unshift(...nextResult.messages);
+    cursor = nextResult.response_metadata.next_cursor;
+  }
+  console.log("messages.length: ", messages.length);
+
+  const histories = [];
+  messages.reverse().forEach((m) => {
+    if (isUserMessage(m)) {
+      histories.push({
+        type: "chat",
+        user: m.user,
+        date: DateTime.fromSeconds(Number(m.ts))
+          .setZone("Asia/Tokyo")
+          .toISODate(),
+      });
+    } else if (isHuddleMessage(m)) {
+      m.room.participant_history.forEach((u) => {
+        histories.push({
+          type: "huddle",
+          user: u,
+          date: DateTime.fromSeconds(Number(m.ts))
+            .setZone("Asia/Tokyo")
+            .toISODate(),
+        });
+      });
+    }
+  });
+  console.log(histories.slice(-20));
+
+  return histories;
 };
 
 const getUserInfos = async (users) => {
